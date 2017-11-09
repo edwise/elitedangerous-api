19972d29f031e7979f5a982dc46f9f3877e7e3e0
edwise <eantonc@gmail.com>
2017-11-09 11:17:09 +0100

    Adds the faction bean and test parse to it with jackson

--- src/main/java/com/edwise/elitedangerous/config/AppStartupRunner.java
+++ src/main/java/com/edwise/elitedangerous/config/AppStartupRunner.java
@@ -1,7 +1,10 @@
 package com.edwise.elitedangerous.config;
 
+import com.edwise.elitedangerous.bean.Faction;
 import com.edwise.elitedangerous.config.bean.EddbConfig;
 import com.edwise.elitedangerous.service.DownloadService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -22,10 +26,13 @@ public class AppStartupRunner implements ApplicationRunner {
 
     private final DownloadService downloadService;
 
+    private ObjectMapper objectMapper;
+
     @Autowired
-    public AppStartupRunner(EddbConfig eddbConfig, DownloadService downloadService) {
+    public AppStartupRunner(EddbConfig eddbConfig, DownloadService downloadService, ObjectMapper objectMapper) {
         this.eddbConfig = eddbConfig;
         this.downloadService = downloadService;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -33,6 +40,9 @@ public class AppStartupRunner implements ApplicationRunner {
         Optional<String> fileContent =
                 downloadService.downloadFileTextContent(eddbConfig.getUrl() + eddbConfig.getFactionsFile());
 
-        log.info("Size of json downloaded: {}", fileContent.map(String::length).orElse(-1));
+        CollectionType javaType = objectMapper.getTypeFactory()
+                                              .constructCollectionType(List.class, Faction.class);
+        List<Faction> factions = objectMapper.readValue(fileContent.orElse("[]"), javaType);
+        log.info("Size of json downloaded: {}", factions.size());
     }
 }
