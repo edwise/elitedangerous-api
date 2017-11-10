d313bfbddceee4c30f6ed68f3b4b9baafbbb45b3
Eduardo Antón <eantonc@gmail.com>
2017-11-10 11:39:44 +0100

    Fixes some deprecated uses of mockito

--- src/test/java/com/edwise/elitedangerous/service/impl/DownloadServiceImplTest.java
+++ src/test/java/com/edwise/elitedangerous/service/impl/DownloadServiceImplTest.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -13,8 +13,8 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
