be1b91e46e25808ef480319cbaf43423ac03f7b4
edwise <eantonc@gmail.com>
2017-11-11 19:13:22 +0100

    Adds method for get what I called the "closest lonely systems" pairs (systems that are close and doesn't have any other system close to)

--- src/main/java/com/edwise/elitedangerous/repository/SystemRepository.java
+++ src/main/java/com/edwise/elitedangerous/repository/SystemRepository.java
@@ -1,6 +1,7 @@
 package com.edwise.elitedangerous.repository;
 
 import com.edwise.elitedangerous.bean.System;
+import com.edwise.elitedangerous.bean.SystemPair;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface SystemRepository {
     void fillData(List<System> systemsList);
 
     double calculateDistance(System system1, System system2);
+
+    List<SystemPair> getClosestLonelySystems();
 }
--- src/test/java/com/edwise/elitedangerous/repository/impl/SystemRepositoryImplTest.java
+++ src/test/java/com/edwise/elitedangerous/repository/impl/SystemRepositoryImplTest.java
@@ -1,18 +1,20 @@
 package com.edwise.elitedangerous.repository.impl;
 
 import com.edwise.elitedangerous.bean.System;
+import com.edwise.elitedangerous.bean.SystemPair;
 import com.edwise.elitedangerous.repository.SystemRepository;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
 
 public class SystemRepositoryImplTest {
     private static final double LIGHT_YEARS_BETWEEN_SOL_AND_ALPHA_CENTAURI = 4.38D;
-    private static final double LIGHT_YEARS_BETWEEN_ODUDURO_AND_SIKSIKAS = 163.91D;
+    private static final double LIGHT_YEARS_BETWEEN_ODUDURO_AND_SIKSIKAS   = 163.91D;
 
     private SystemRepository systemRepository;
 
@@ -45,6 +47,18 @@ public class SystemRepositoryImplTest {
         double result = systemRepository.calculateDistance(system1, system2);
 
         assertThat(result).isCloseTo(LIGHT_YEARS_BETWEEN_ODUDURO_AND_SIKSIKAS, within(0.005D));
+    }
+
+    @Test
+    public void getClosestLonelySystemsShouldReturnPairOfClosestLonelySystem() {
+        SystemPair expectedResult = new SystemPair(createSOLSystem(), createAlphaCentauriSystem());
+        systemRepository.fillData(Arrays.asList(createSOLSystem(), createAlphaCentauriSystem(),
+                                                createSiksikasSystem(), createOduduroSystem()));
+
+        List<SystemPair> closestLonelySystems = systemRepository.getClosestLonelySystems();
+
+        assertThat(closestLonelySystems).containsExactly(expectedResult);
+
     }
 
     private System createSiksikasSystem() {
