2529f176a9cf2e7c09464dc56d96941bbe93c2a0
edwise <eantonc@gmail.com>
2017-11-14 21:49:32 +0100

    Adds new controller for SystemPair

--- src/main/java/com/edwise/elitedangerous/controller/SystemPairController.java
+++ src/main/java/com/edwise/elitedangerous/controller/SystemPairController.java
@@ -1,5 +1,6 @@
 package com.edwise.elitedangerous.controller;
 
+import com.edwise.elitedangerous.bean.SystemPair;
 import com.edwise.elitedangerous.service.SystemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,23 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/systems")
-public class SystemsController {
+@RequestMapping("/api/systempair")
+public class SystemPairController {
 
     private SystemService systemService;
 
     @Autowired
-    public SystemsController(SystemService systemService) {
+    public SystemPairController(SystemService systemService) {
         this.systemService = systemService;
     }
 
     @GetMapping
-    public ResponseEntity<List<System>> getAllSystems() {
-        // TODO implement ?
-        return new ResponseEntity<>(Arrays.asList(), HttpStatus.OK);
+    public ResponseEntity<List<SystemPair>> getAllSystems() {
+        List<SystemPair> systemPairs = systemService.obtainClosestLonelySystems();
+        return new ResponseEntity<>(systemPairs, HttpStatus.OK);
     }
 }
